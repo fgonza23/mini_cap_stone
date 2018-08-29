5bf2da29a671a67326a50661f04e02524ffc957e
fgonza23 <fgonza0267@gmail.com>
2018-08-29 17:43:17 -0500

    updated routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,5 +2,12 @@ class Api::ProductsController < ApplicationController
   def all_products_action
     @products = Product.all
     render 'all_products_view.json.jbuilder'
+  end
+
+  def first_product_action
+    @Product = Product.first_product
+    render 'first_product.json.jbuilder'
+  end
 end
+
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace: api do
-    get"/all_products_url" => "products#all_products_action"
+    get "/all_products_url" => "products#all_products_action"
+    get "/first_product_url" => 'products#first_product_action'
   end
 
 end
