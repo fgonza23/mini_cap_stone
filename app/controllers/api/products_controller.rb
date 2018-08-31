42a9bf2be8fbb8d60841120d4464d4e2952f5f6d
fgonza23 <fgonza0267@gmail.com>
2018-08-30 21:10:57 -0500

    updated controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -21,6 +21,24 @@ class Api::ProductsController < ApplicationController
     @product .save
     render 'show.json.jbuilder'
   end
+
+  def update
+    @product = Product.find(params[:id])
+
+    @product.name = params[:id] || @product.name
+    @product.price = params[:id] || @product.price
+    @product.image_url = params[:id] || @product.image_url
+    @product.description = params[:id] || @product.description
+
+    @product.save
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    render json: {message: "Product successfully destroyed"}
+  end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   namespace:api do
 
     get "/products" => 'products#index'
-    get "/products/:id" => 'products#show'
     post "/products" => 'products#create'
+    get "/products/:id" => 'products#show'
+    patch "/products/:id" => 'products#update'
+    delete "/products/:id" => 'products#destroy'
   end
 
 end
