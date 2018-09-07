601ebd613e3111df4bf99542c120d3746d84f94e
fgonza23 <fgonza0267@gmail.com>
2018-09-06 21:31:22 -0500

    update mini cap

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,14 @@
 class Product < ApplicationRecord
  validates :name, presence: true
  validates :name, uniqueness: true
- validates :descpription, presence: true
- validates :descpription, length: { minimum: 20 }
- validates :descpription, length: { maximum: 200 }
+ validates :description, presence: true
+ validates :description, length: { minimum: 20 }
+ validates :description, length: { maximum: 200 }
  validates :image_url, uniqueness: true
  validates :price, presence: true
  validates :price, numericality: { greater_than: 0 }
+
+  belongs_to :supplier
 
 
   def is_discounted? 
